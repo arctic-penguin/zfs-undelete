76ac92e660dd4bce525b5887cfaa8d03ab0800fb
arctic-penguin <103587359+arctic-penguin@users.noreply.github.com>
2022-11-14 14:12:07 +0100

    add Arguments struct

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,39 @@
 use std::env::args;
 use std::path::PathBuf;
 
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
 
-pub(crate) fn get_filename_from_args() -> Result<PathBuf> {
-    Ok(args().nth(1).with_context(|| "filename expected")?.into())
+pub(crate) struct Arguments {
+    pub(crate) mode: Mode,
+    pub(crate) filename: PathBuf,
+}
+
+pub(crate) enum Mode {
+    MostRecentVersion,
+    AllVersions,
+}
+
+impl Mode {
+    fn get_from_args(args: &mut Vec<String>) -> Self {
+        if args[0] == "-V" {
+            args.pop().expect("there's at least one element");
+            Self::AllVersions
+        } else {
+            Self::MostRecentVersion
+        }
+    }
+}
+
+impl Arguments {
+    pub(crate) fn get_args() -> Result<Self> {
+        let mut raw_args: Vec<_> = args().skip(1).rev().collect();
+        if raw_args.is_empty() {
+            bail!("Missing arguments");
+        }
+        let mode = Mode::get_from_args(&mut raw_args);
+        let filename = raw_args.pop().context("filename missing")?.into();
+        Ok(Self { mode, filename })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,15 @@ mod undelete;
 mod zfs;
 
 use anyhow::{bail, Result};
-
-use crate::path::Absolute;
+use path_absolutize::Absolutize;
 
 fn main() -> Result<()> {
-    let to_recover = args::get_filename_from_args()?;
-    if to_recover.exists() {
+    let arguments = args::Arguments::get_args()?;
+    if arguments.filename.exists() {
         bail!("Cannot restore already existing file.");
     }
 
-    let to_recover_absolute = to_recover.make_absolute()?;
+    let to_recover_absolute = arguments.filename.absolutize()?;
 
     let dataset = zfs::Dataset::find(&to_recover_absolute)?;
     let to_recover_relative_to_mountpoint = dataset.get_relative_path(&to_recover_absolute);
