d39922186144cd32d6bf75505f83501d4bdff3a9
arctic-penguin <103587359+arctic-penguin@users.noreply.github.com>
2022-11-29 14:48:55 +0100

    add docstrings

--- src/config.rs
+++ src/config.rs
@@ -11,6 +11,7 @@ pub(crate) struct Config {
 }
 
 impl Config {
+    /// Load the program config from file. Apply defaults if no value provided for a config key.
     pub(crate) fn load() -> Result<Self> {
         let conf_file = Self::get_config_file().context("could not find config file")?;
         let content = fs::read_to_string(&conf_file)
@@ -28,12 +29,15 @@ impl Config {
         this.sanity_checked()
     }
 
+    /// Get the path of the config file like. This is `$XDG_CONFIG_HOME/zfs-undelete.conf`.
     fn get_config_file() -> Result<PathBuf> {
         let mut conf_dir = get_xdg_config_home().context("could not get XDG_CONFIG_HOME")?;
         conf_dir.push("zfs-undelete.conf");
         Ok(conf_dir)
     }
 
+    /// Takes ownership of the instance and returns itself, but sanity-checked and wrapped in
+    /// `anyhow::Result<_>`.
     fn sanity_checked(self) -> Result<Self> {
         if self.ls_command.is_empty() {
             bail!("missing value for LsCommand");
@@ -52,6 +56,7 @@ fn get_xdg_config_home() -> Result<PathBuf> {
     }
 }
 
+/// Get the home-directory of the current user by looking up the `$HOME` environment variable.
 fn get_home_dir() -> Result<PathBuf> {
     Ok(env::var("HOME").context("$HOME not declared")?.into())
 }
